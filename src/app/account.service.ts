0a25977b98bcf49750fd465c5cd4bdd2e30240c8
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-15 12:02:09 +0300

    Add a binding method to the search button to execute the search functionality

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class AccountService {
 
   accountDetails!: User;
-  repos: Repo;
+  repos!: Repo;
   clientid: string = "3dd2666d6db96a58e0aa"
   clientsecret: string = "8951785aaf5c1c732b1092ae0671304fc1a28975"
 
@@ -18,7 +18,7 @@ export class AccountService {
 
   constructor(private http: HttpClient) {
     this.accountDetails = new User("", "", "", "", "", 0, 0, 0, 0, "", "", "", new Date);
-    this.repos = [];
+    // this.repos = [];
   }
 
   getUser(userName: string) {
@@ -37,7 +37,7 @@ export class AccountService {
       blog:string;
       created_at:Date;
     }
-    let promise = new promise<void>((resolve, reject) =>{
+    let promise: any = new Promise<void>((resolve, reject) =>{
       let apiUrl = this.url + userName + '?client_id='+this.clientid+'&client_secret='+this.clientsecret
       this.http.get<ApiResponse>(apiUrl)
         .toPromise()
@@ -56,8 +56,23 @@ export class AccountService {
             this.accountDetails.email = res!.email
             this.accountDetails.blog = res!.blog
             this.accountDetails.created_at = res!.created_at
-          }
-        )
+
+            resolve();
+          }, error => {
+            this.accountDetails.login = "An error occured!"
+            this.accountDetails.url = "An error occured!"
+
+            reject(error)
+          })
     })
+    console.log(promise)
+    return promise
+  }
+
+  getRepos(userName: string): Observable<Repo[]> {
+
+    let apiUrl = this.url + userName + '/repos?client_id='+this.clientid+'&client_secret='+this.clientsecret
+    return this.http.get<Repo[]>
+      (apiUrl)
   }
 }
