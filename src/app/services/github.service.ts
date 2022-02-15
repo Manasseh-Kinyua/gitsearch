4d4e3dd5b6297ad264cf9797534a6679704a24b1
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-15 06:17:25 +0300

    Add a function to handle the errors

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -13,8 +13,22 @@ export class GithubService {
   //github profile
   public getProfile(searchQuery) {
     let dataUrl = `https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
-    return this.httpClient.get()
+    return this.httpClient.get(dataUrl).pipe(
+      retry(count 1)
+      catchError(this.handleErrors)
+    )
   }
 
   //github repos
+  public handleErrors(error: HttpErrorResponse) {
+    let erroMessage: string;
+    if(error.error instanceof ErrorEvent) {
+      //client side error
+      errorMessage = `MESSAGE : ${error.error.message}`;
+    } else {
+      //server side error
+      erroMessage = `STATUS : ${error.status} MESSAGE : ${error.message}`
+    }
+    return throwError(erroMessage)
+  }
 }
