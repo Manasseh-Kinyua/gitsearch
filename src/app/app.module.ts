dc608fc2da935989a513eb60e0b3e5d37bc0cfea
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-15 05:25:28 +0300

    Add the initial file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubReposComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/credentials/GithubCred.ts
@@ -0,0 +1,2 @@
+export const CLIENT_ID = "3dd2666d6db96a58e0aa"
+export const CLIENT_SECRET = "8951785aaf5c1c732b1092ae0671304fc1a28975"
