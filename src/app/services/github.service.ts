78694c8522b5ef1f2141c8901080ecfa8f661a95
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-15 06:01:27 +0300

    Add a getProfile method to fetch the user's profile

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CLIENT_ID } from '../credentials/GithubCred';
+import { CLIENT_SECRET } from '../credentials/GithubCred';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  //github profile
+  public getProfile(searchQuery) {
+    let dataUrl = `https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    return this.httpClient.get()
+  }
+
+  //github repos
 }
